864d6a5e7811db8babba8c0c07be9ecbc7371867
Chloe Pratas <chloevison97@gmail.com>
2023-06-20 01:41:59 +0200

    Last test Hopefuly

--- ems-app/src/app/api/clerk/webhook/route.ts
+++ ems-app/src/app/api/clerk/webhook/route.ts
@@ -1,10 +1,14 @@
 export async function POST(req: Request) {
   try {
     const token = req.headers.get("Bearer");
-    console.log(token, token === process.env.CLERK_SECRET_KEY);
+    if (token !== process.env.CLERK_SECRET_KEY) {
+      throw new Error("Invalid token");
+    }
+    const payload = await req.json();
+    console.log(payload);
     return new Response("POST");
   } catch (error) {
     console.log(error);
-    return new Response("POST");
+    return new Response("POST", { status: 401 });
   }
 }
