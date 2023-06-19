f6a4ae2ebab12aa0e70dfd53bd8b7ae5a9cffdbd
Chloe Pratas <chloevison97@gmail.com>
2023-06-20 01:36:39 +0200

    WorkAround for webhook AUTH

--- ems-app/src/app/api/clerk/webhook/route.ts
+++ ems-app/src/app/api/clerk/webhook/route.ts
@@ -1,35 +1,8 @@
-import crypto from "crypto";
-
 export async function POST(req: Request) {
   try {
-    const svix_id = req.headers.get("svix-id");
-    const svix_timestamp = req.headers.get("svix-timestamp");
-    const secretBytes = new Buffer(
-      process.env.WEB_HOOKS!.split("_")[1],
-      "base64"
-    );
-    const signedContent = `${svix_id}.${svix_timestamp}.${req.body}`;
-    const signature = crypto
-      .createHmac("sha256", secretBytes)
-      .update(signedContent)
-      .digest("base64");
-    const test = signature === req.headers.get("svix-signature");
-    // const payload = req.body;
-    // console.log(payload);
-
-    // const headers = req.headers;
-    // console.log(headers);
-
-    // const wh = new Webhook(process.env.CLERK_SECRET_KEY!);
-    // let msg;
-
-    // msg = wh.verify(payload, headers);
-
-    // // // Do something with the message...
-    console.log(test);
-    return new Response(
-      JSON.stringify({ message: test, req: svix_id, svix_timestamp })
-    );
+    const token = req.headers.get("Bearer");
+    console.log(token, token === process.env.CLERK_SECRET_KEY);
+    return new Response("POST");
   } catch (error) {
     console.log(error);
     return new Response("POST");
