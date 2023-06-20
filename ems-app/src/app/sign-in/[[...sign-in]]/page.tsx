e864f2d33f698281b8ce71a1073fc8715f4a6f7c
Chloe Pratas <chloevison97@gmail.com>
2023-06-20 23:15:06 +0200

    Sign in and sign up pages

--- ems-app/src/app/sign-in/[[...sign-in]]/page.tsx
+++ ems-app/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,15 @@
+import Header from "@/components/hero/Header";
 import { SignIn } from "@clerk/nextjs";
 
 type Props = {};
 function page({}: Props) {
-  return <SignIn />;
+  return (
+    <>
+      <Header />
+      <main className="flex-1 flex justify-center items-center">
+        <SignIn />
+      </main>
+    </>
+  );
 }
 export default page;
