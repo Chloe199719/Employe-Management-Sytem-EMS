477d3e42f80ef6fd2ec6396e42177e5eda8f19eb
Chloe Pratas <chloevison97@gmail.com>
2023-06-20 02:07:53 +0200

    Testing webhook

--- ems-app/src/app/api/clerk/webhook/route.ts
+++ ems-app/src/app/api/clerk/webhook/route.ts
@@ -1,13 +1,33 @@
+import clerk, { WebhookEvent } from "@clerk/clerk-sdk-node";
+import prismaClient from "@/lib/prisma/prisma";
 export async function POST(req: Request) {
   try {
     const token = req.headers.get("Bearer");
-    if (token !== process.env.CLERK_SECRET_KEY) {
-      throw new Error("Invalid token");
+    if (token === process.env.CLERK_SECRET_KEY) {
+      throw new Error("Invalid token", { cause: "Invalid token" });
     }
-    const payload = await req.json();
-    console.log(payload);
+    const payload = (await req.json()) as WebhookEvent;
+    if (payload.data.id === undefined) {
+      throw new Error("No ID in Request", { cause: "Missing ID" });
+    }
+    const create = await prismaClient.user.create({
+      data: {
+        userID: payload.data.id!,
+      },
+    });
+    const metadata = await clerk.users.updateUser(payload.data.id, {
+      privateMetadata: {
+        role: "guest",
+      },
+    });
+    console.log(payload.data);
     return new Response("POST");
-  } catch (error) {
+  } catch (error: any) {
+    if (error.message === "Invalid token") {
+      return new Response("Invalid token", { status: 401 });
+    } else if (error.message === "Missing ID") {
+      return new Response("No ID in Request", { status: 400 });
+    }
     console.log(error);
     return new Response("POST", { status: 401 });
   }
