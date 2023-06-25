79f6c27ed8b982e3345d1ce383f03c6ede165a44
Chloe Pratas <chloevision97@gmail.com>
2023-06-26 00:13:21 +0200

    Fixing the team task bug

--- ems-app/src/app/dashboard/admin/teams/[teamid]/loading.tsx
+++ ems-app/src/app/dashboard/admin/teams/[teamid]/loading.tsx
@@ -1,16 +1,15 @@
+import Canvas from "@/components/Dashboard/Canvas";
 import LoadingNavBar from "@/components/Loading/LoadingNavBar";
 import LoadingTeam from "@/components/Loading/LoadingTeam";
 
 type Props = {};
 function loading({}: Props) {
   return (
-    <div className="flex flex-1 flex-col p-4">
-      <div className="bg-base-100 p-4 rounded-lg flex flex-col gap-8 flex-1">
-        <LoadingNavBar />
-        <hr />
-        <LoadingTeam />
-      </div>
-    </div>
+    <Canvas>
+      <LoadingNavBar />
+      <hr />
+      <LoadingTeam />
+    </Canvas>
   );
 }
 export default loading;
--- ems-app/src/components/Dashboard/Canvas.tsx
+++ ems-app/src/components/Dashboard/Canvas.tsx
@@ -4,7 +4,7 @@ type Props = {
 function Canvas({ children }: Props) {
   return (
     <div className="flex flex-1 flex-col p-6 pb-8">
-      <div className="bg-base-100 p-4 rounded-lg flex flex-col gap-8 flex-1">
+      <div className="bg-base-100 p-4 pt-8 rounded-lg flex flex-col gap-8 flex-1">
         {children}
       </div>
     </div>
